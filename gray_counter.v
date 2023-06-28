a8cfce8e6a14225af54fc92ab0dc848a6d99e903
Shubham Kumar <112322996+Shubham6359@users.noreply.github.com>
2023-06-28 09:49:41 +0530

    Create gray_counter.v

---
+++ gray_counter.v
@@ -0,0 +1,14 @@
+module gray_counter(clk, rst, enable, out);
+  input clk, rst, enable;
+  output [7:0] out;
+  wire [7:0] out;
+  reg [7:0] count;
+
+  always@(posedge clk)
+    if(rst)
+        count <= 0;
+    else if(enable)
+        count <= count + 1;
+  assign out = {count[7], (count[7]^count[6]), (count[6]^count[5]), (count[5]^count[4]), (count[4]^count[3]), (count[3]^count[2]), (count[2]^count[1]), (count[1]^count[0])};
+
+endmodule     
