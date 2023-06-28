c2e6ea3fb3690a2b4ead494edfbfa348471e7610
Shubham Kumar <112322996+Shubham6359@users.noreply.github.com>
2023-06-28 09:52:37 +0530

    Update gray_counter.v

--- gray_counter.v
+++ gray_counter.v
@@ -9,6 +9,6 @@ module gray_counter(clk, rst, enable, out);
         count <= 0;
     else if(enable)
         count <= count + 1;
-  assign out = {count[7], (count[7]^count[6]), (count[6]^count[5]), (count[5]^count[4]), (count[4]^count[3]), (count[3]^count[2]), (count[2]^count[1]), (count[1]^count[0])};
+    assign out = {count[7], (count[7]^count[6]), (count[6]^count[5]), (count[5]^count[4]), (count[4]^count[3]), (count[3]^count[2]), (count[2]^count[1]), (count[1]^count[0])};
 
-endmodule     
+endmodule
