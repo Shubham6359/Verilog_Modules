ba70d7a1275fa254206215c238eddf31c48b2d9f
Shubham Kumar <112322996+Shubham6359@users.noreply.github.com>
2023-07-11 11:41:05 +0530

    Create sequence_detector_tb.v

---
+++ sequence_detector_tb.v
@@ -0,0 +1,23 @@
+module sequence_detector_tb;
+  reg din, clk, rst;
+  wire y;
+  sequence_detector_tb uut(.din(din),.clk(clk),.rst(rst));
+
+  initial begin
+    rst = 0;
+    din = 0;
+    clk = 0;
+    
+    #5 din=1;
+    #5 din=1;
+    #5 din=1;
+    #10 din=0;
+    #10 din=1;
+    #10 din=0;
+    #10 din=1;
+    #10 din=0;
+    #5 din=1;
+  end
+  always #5 clk = ~clk;
+  initial #100 $finish;
+endmodule
